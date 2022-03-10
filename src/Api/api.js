1a8ca5137c6c447829e321809b89cc89e9ef3f0d
A-raniy-day <70303120+A-raniy-day@users.noreply.github.com>
2022-03-10 20:18:49 +0800

    Delete src directory

--- src/Api/api.js
+++
@@ -1,9 +0,0 @@
-//import axios from "axios";
-const search = async q => {
-    const res = await fetch(`http://localhost:3001/search?q=${q}`)
-    console.log(res)
-    const response = await res
-    return response.data
-}
-
-module.exports.search = search
